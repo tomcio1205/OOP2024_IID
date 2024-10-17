740a8846d6e2f06158526a0db3f9835880f93c7a
Tomasz Rajzer <trajzer@wsiz.edu.pl>
2024-10-17 11:23:50 +0200

    first commit

---
+++ OOP/Constructions/Construction.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP.Constructions
+{
+    internal class Construction
+    {
+        public Construction()
+        {
+                
+        }
+
+        public Construction(float height, float width, int entrances, int humanCapacity, string buildMaterial)
+        {
+            Height = height; 
+            Width = width; 
+            Entrances = entrances; 
+            HumanCapacity = humanCapacity; 
+            BuildMaterial = buildMaterial; 
+        }
+
+        public float Height { get; set; }
+        public float Width { get; set; }
+        public int Entrances { get; set; }
+        public int HumanCapacity { get; set; }
+        public string BuildMaterial { get; set; }
+    }
+}
---
+++ OOP/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using OOP.Constructions;
+
+Console.WriteLine("Hello, World!");
+var construction1 = new Construction();
+var construction2 = new Construction(23.0f, 45.0f, 4, 4, "Material");
+Console.WriteLine("Hello, World!");
